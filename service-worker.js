7be86d211b7af13a51f671159e641f6e4cb06101
cheztvplus-tech <cheztvplus@gmail.com>
2026-01-21 15:41:20 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -19,11 +19,12 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 
 });
+
 
 
