90e8a0adee109809f52538aac8ef3a65738b5eda
cheztvplus-tech <cheztvplus@gmail.com>
2026-01-21 22:05:23 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,27 @@
-// service-worker.js (DEV MODE - NO CACHING)
+const CACHE_NAME = "domino-tracker-cache-v1.001";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./tiles-black/",
+  "./tiles-white/",
+  "./tiles-red/",
+  "./tiles-green/",
+  "./tiles-purple/",
+  "./sets.json"
+];
 
-self.addEventListener("install", () => {
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", () => {
-  self.clients.claim();
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+
 });
-
-// 🚫 No fetch handler = no caching
-
-
-
-
-
-
-
-//const CACHE_NAME = "domino-tracker-cache-v1.01";
-//const urlsToCache = [
- // "./",
-//  "./index.html",
-//  "./style.css",
-//  "./script.js",
-//  "./manifest.json",
-//  "./tiles-black/",
-//  "./tiles-white/",
-//  "./tiles-red/",
-//  "./tiles-green/",
-//  "./tiles-purple/",
-//  "./sets.json"
-//];
-
-//self.addEventListener("install", (event) => {
- // event.waitUntil(
-   // caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-//  );
-//});
-
-//self.addEventListener("fetch", (event) => {
-//  event.respondWith(
- //   caches.match(event.request).then((resp) => resp || fetch(event.request))
- // );
-
-//});
